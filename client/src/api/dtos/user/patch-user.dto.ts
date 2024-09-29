7342cc3abb799a749c1408a9072608a2a96a6556
dmhaui <dinhnhatminh.minhkhanh@gmail.com>
2024-09-29 22:50:07 +0700

    dtos

--- client/src/api/dtos/user/patch-user.dto.ts
+++ client/src/api/dtos/user/patch-user.dto.ts
@@ -1,7 +1,29 @@
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 import { PartialType, OmitType } from '@nestjs/swagger';
 
 export class PatchUserDto extends PartialType(
   OmitType(CreateUserDto, ['password', 'username'] as const)
-) {}
+) {
+  @IsNumber()
+  @IsOptional()
+  age: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(96)
+  firstName: string;
+
+  @IsString()
+  @IsOptional()
+  @MinLength(3)
+  @MaxLength(96)
+  lastName?: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  @MaxLength(96)
+  email: string;
+
+}
